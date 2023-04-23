276a785a369406f4e2ba5ee994d3c4a5bc839662
Ranianirudh <131569136+Ranianirudh@users.noreply.github.com>
2023-04-23 09:45:21 -0700

    Update Student.java

--- Student.java
+++ Student.java
@@ -3,11 +3,13 @@ class Student{
   String name;
   float marks;
   public void reading(){
+    System.out.println("I am reading");
   }
   public void writing(){
+    System.out.println("I am writing");
   }
   public static void main(String args[]){
-    System.out.println("");
+    
   }
 }
   
