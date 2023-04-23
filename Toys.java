74902c7b1492dcebab57c07687f2a54c9db2ead0
Ranianirudh <131569136+Ranianirudh@users.noreply.github.com>
2023-04-23 09:47:01 -0700

    Update Toys.java

--- Toys.java
+++ Toys.java
@@ -2,8 +2,10 @@ public class Toys{
   float price;
   String name;
   public void play(){
+    System.out.println("Play with Toys");
   }
   public void music(){
+    System.out.println("listening music while playing");
   }
   public static void main(String args[]){
     System.out.println("");
