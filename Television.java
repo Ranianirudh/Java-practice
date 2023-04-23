a120bd2b511d4dd3866c9b3a25d9c3afa3e30cf8
Ranianirudh <131569136+Ranianirudh@users.noreply.github.com>
2023-04-23 00:46:41 -0700

    Creating Television Class without Main()

---
+++ Television.java
@@ -0,0 +1,11 @@
+public class Television{
+  int size;
+  float price;
+  String brandName;
+  public void playMusic(){
+    System.out.println("playing your favourite song");
+  }
+  public void playMovie(){
+    System.out.println("playing movie");
+  }
+}
