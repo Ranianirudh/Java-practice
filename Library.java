902f13f7471336b1d30b133a677e7363b813c671
Ranianirudh <131569136+Ranianirudh@users.noreply.github.com>
2023-04-23 00:59:43 -0700

    Creating Library class

---
+++ Library.java
@@ -0,0 +1,10 @@
+public class Library{
+  int noOfBooks;
+  String authorName;
+  public void collectBooks(){
+    System.out.println("collecting books");
+  }
+  public void returnBooks(){
+    System.out.println("i returned the book");
+  }
+}
