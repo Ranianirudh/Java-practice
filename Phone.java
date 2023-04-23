7c8ae35218f6354e559c6a6a6138717eaea4f7b5
Ranianirudh <131569136+Ranianirudh@users.noreply.github.com>
2023-04-23 00:34:26 -0700

    Creating Phone Class

---
+++ Phone.java
@@ -0,0 +1,14 @@
+public class Phone{
+  int number;
+  String brand;
+  public void call(){
+  }
+  public void sendMsg(){
+  }
+  public void receiveMsg(){
+  }
+  public static void main(String args[]){
+    System.out.println("");
+  }
+}
+    
