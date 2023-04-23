f303b8d28d1fb2ed1485d7a4325eb1c1da4bc85c
Ranianirudh <131569136+Ranianirudh@users.noreply.github.com>
2023-04-23 01:06:58 -0700

    Update Laptop.java
    I added printing statement to methods.

--- Laptop.java
+++ Laptop.java
@@ -3,13 +3,16 @@ public class Laptop{
   String processor;
   int ramCapacity;
   public void getInput{
+     System.out.println("read the data");
   }
   public void process{
+     System.out.println("process the data");
   }
   public void output{
+     System.out.println("result");
   }
   public static void main(String args[]){
-    System.out.println("");
+   
   }
 }
   
