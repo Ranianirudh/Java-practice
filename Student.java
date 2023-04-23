c3fd091bb09cea0a28e614329dab72f0d6d53820
Ranianirudh <131569136+Ranianirudh@users.noreply.github.com>
2023-04-22 23:31:57 -0700

    Creating Student class

---
+++ Student.java
@@ -0,0 +1,12 @@
+class Student{
+  int rno;
+  String name;
+  float marks;
+  public reading(){
+  }
+  public writing(){
+  }
+  public static void main(String args[]){
+    System.out.println("");
+  }
+  
