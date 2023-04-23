948d2f0f378de1f81e58a4a0f14a0a33f217e9b4
Ranianirudh <131569136+Ranianirudh@users.noreply.github.com>
2023-04-23 00:39:39 -0700

    Creating Toys Class

---
+++ Toys.java
@@ -0,0 +1,11 @@
+public class Toys{
+  float price;
+  String name;
+  public void play(){
+  }
+  public void music(){
+  }
+  public static void main(String args[]){
+    System.out.println("");
+  }
+}
