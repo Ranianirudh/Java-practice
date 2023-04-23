30940571810ef378c932e2a1754be78e9d0f3e18
Ranianirudh <131569136+Ranianirudh@users.noreply.github.com>
2023-04-23 00:13:46 -0700

    Creating Laptop class

---
+++ Laptop.java
@@ -0,0 +1,15 @@
+public class Laptop{
+  int size;
+  String processor;
+  int ramCapacity;
+  public void getInput{
+  }
+  public void process{
+  }
+  public void output{
+  }
+  public static void main(String args[]){
+    System.out.println("");
+  }
+}
+  
