ef71202316ed28dde018c3051a36a93125c9e8cc
Ranianirudh <131569136+Ranianirudh@users.noreply.github.com>
2023-04-22 23:47:49 -0700

    creating Person class

---
+++ Person.java
@@ -0,0 +1,15 @@
+public class Person{
+  String name;
+  float height;
+  float weight;
+  public working(){
+  }
+  public reading(){
+  }
+  public sleeping(){
+  }
+  public static void main(String args[]){
+  System.out.println("");
+    }
+}
+  
