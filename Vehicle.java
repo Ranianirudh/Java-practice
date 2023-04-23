7f0d0e363ef6156cb9f3e6f1a13a75da2d7b807c
Ranianirudh <131569136+Ranianirudh@users.noreply.github.com>
2023-04-23 09:49:50 -0700

    Update Vehicle.java

--- Vehicle.java
+++ Vehicle.java
@@ -2,8 +2,10 @@ public class Vehicle{
   int wheels;
   String modeOfTransport;
   public void driving(){
+     System.out.println("I am Driving");
   }
   public void honk(){
+     System.out.println("Honk");
   }
   public static void main(String args[]){
     System.out.println("");
