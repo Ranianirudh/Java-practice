8cf9f27020af81c755221ccfe819183c41ae29c4
Ranianirudh <131569136+Ranianirudh@users.noreply.github.com>
2023-04-23 00:26:03 -0700

    Creating Vehicle Class

---
+++ Vehicle.java
@@ -0,0 +1,11 @@
+public class Vehicle{
+  int wheels;
+  String modeOfTransport;
+  public void driving(){
+  }
+  public void honk(){
+  }
+  public static void main(String args[]){
+    System.out.println("");
+}
+}
