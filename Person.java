3b64fc48ce79e9bd8b491f5c2293f4d159ace963
Ranianirudh <131569136+Ranianirudh@users.noreply.github.com>
2023-04-23 01:08:33 -0700

    Update Person.java
    I added printing statement to methods.

--- Person.java
+++ Person.java
@@ -3,13 +3,16 @@ public class Person{
   float height;
   float weight;
   public void working(){
+    System.out.println("");
   }
   public void reading(){
+    System.out.println("");
   }
   public void sleeping(){
+    System.out.println("");
   }
   public static void main(String args[]){
-  System.out.println("");
+  
     }
 }
   
